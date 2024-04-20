f009d5179d8b979be021dd7f4b3f80bd663977c3
jamesduran <james_duran@fastmail.com>
2024-04-19 21:27:16 -0700

    Added main.c structure

--- src/main.c
+++ src/main.c
@@ -1,6 +1,21 @@
 #include<stdio.h>
+#include<stdbool.h>
+
+typedef struct {
+
+} World;
 
 int main() {
-	printf("Hello, World!\n");
+	InitWindow(1920, 1080, "Physics Demo", NULL, NULL);
+
+	World *world = WorldInit();
+
+	while (!WindowShouldClose()) {
+		WorldTick(world);
+		VisDraw(world);
+	}
+
+	WorldDeinit(world);
+	CloseWindow();
 	return 0;
 }
