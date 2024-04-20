ac5a8accd5d746aec9b58b73bd4620702560aad7
yogichip <151203539+yogichip@users.noreply.github.com>
2024-04-20 16:13:49 -0700

    border detection

--- src/main.c
+++ src/main.c
@@ -5,43 +5,61 @@
 #include "raylib.h"
 
 const int width  = 800;
-const int height = 450;
+const int height = 800;
 
 void drawPoly(Vec2* points, size_t size, Vec2* velocity){                      
     for (int i = 0; i<size; i++){                              
 		points[i].x = points[i].x + velocity->x;                                       
 		points[i].y = points[i].y + velocity->y;  
-		if(points[i].x>width){
-			points[i].x = width;
+		if(points[i].x>FixedFromInt(width)){
+			points[i].x = FixedFromInt(width);
 			velocity->x = 0;
 			velocity->y = 0;
-		}	
-		if(points[i].y>height){
-			points[i].y = height;
+		}else if(points[i].x<0){
+			points[i].x = FixedFromInt(0);
+			velocity->x = 0;
+			velocity->y = 0;
+		}
+		if(points[i].y>FixedFromInt(height)){
+			points[i].y = FixedFromInt(height);
 			velocity->x = 0;
 			velocity->y = 0;                     
-		}                       
+		}   else if(points[i].y<0){
+			points[i].y = FixedFromInt(0);
+			velocity->x = 0;
+			velocity->y = 0;
+		}                    
     }       
 	for (int i = 0; i<size-1; i++){                                                                     
-        DrawLine(points[i].x, points[i].y, points[i+1].x, points[i+1].y,BLACK);       
+        DrawLine((int)FloatFromFixed(points[i].x), (int)FloatFromFixed(points[i].y), (int)FloatFromFixed(points[i+1].x), (int)FloatFromFixed(points[i+1].y),BLACK);       
     }    
-    DrawLine(points[0].x, points[0].y, points[size-1].x, points[size-1].y,RED);    
+    DrawLine((int)FloatFromFixed(points[0].x), (int)FloatFromFixed(points[0].y),(int)FloatFromFixed(points[size-1].x), (int)FloatFromFixed(points[size-1].y),RED);    
 }                                                                                     
 
 
 void drawCircle(Vec2 center, Fixed_FLT radius, Vec2* velocity){                
-    center.x = center.x + velocity->x;                                                 
-    center.y = center.y + velocity->y;        
-	if(center.x>width-radius){
-		center.x = width-radius;
+    center= Vec2Add(center,*velocity);
+
+	if(center.x>FixedFromInt(width-radius)){
+		center.x = FixedFromInt(width-radius);
 		velocity->x = 0;
 		velocity->y = 0;
+	}else if(center.x<FixedFromInt(radius)){
+		center.x = FixedFromInt(radius);
+		velocity->x = 0;
+		velocity->y = 0;
+
 	}
-	if(center.y>height-radius){
-		center.y = height-radius;
+	if(center.y>FixedFromInt(height-radius)){
+		center.y = FixedFromInt(height-radius);
 		velocity->x = 0;
 		velocity->y = 0;
-	}                                 
+	} else if(center.y<FixedFromInt(radius)){
+		center.x = FixedFromInt(radius);
+		velocity->x = 0;
+		velocity->y = 0;
+
+	}                              
     DrawCircle((int)FloatFromFixed(center.x),(int)FloatFromFixed(center.y), radius, DARKBLUE);                                            
 }
 
@@ -51,14 +69,17 @@ int main(void) {
 	InitWindow(width, height, "Physics Demo");
 
 	Vec2 velocity = (Vec2){
-		.x = FixedFromInt(0),
-		.y = FixedFromInt(0),
+		.x = FixedFromFloat(-0.1f),
+		.y = FixedFromFloat(-0.1f),
 	};
 
 	Vec2 object1[] = {                                                                
-    (Vec2){ .x = FixedFromInt(100), .y = FixedFromInt(150) },  
-    (Vec2){ .x = FixedFromInt(200), .y = FixedFromInt(150) },                                                              
-    (Vec2){ .x = FixedFromInt(300), .y = FixedFromInt(450) },                                                                                    
+    //(Vec2){ .x = FixedFromInt(100), .y = FixedFromInt(150) },  
+    //(Vec2){ .x = FixedFromInt(200), .y = FixedFromInt(150) },                                                              
+    (Vec2){ .x = FixedFromInt(300), .y = FixedFromInt(450) }, 
+    (Vec2){ .x = FixedFromInt(400), .y = FixedFromInt(460) },                                                                                    
+    (Vec2){ .x = FixedFromInt(600), .y = FixedFromInt(550) },                                                                                    
+
  	};
 
  	int size = sizeof(object1)/sizeof(Vec2);
